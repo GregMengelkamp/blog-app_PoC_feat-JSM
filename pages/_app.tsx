06be515099413f338d33f8fa6f5710e4e5e099af
GregMengelkamp <gmengelkamp@gmail.com>
2022-01-15 15:44:33 +0100

    added type annotation to pass ts-strictmode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import '../styles/globals.scss';
 import { Layout } from '../components';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps } : { Component: any, pageProps: any}) {
   return (
     <Layout>
       <Component {...pageProps} />
